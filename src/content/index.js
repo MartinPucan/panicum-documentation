d9f87a6b508f8d3cc1857e03657d2f758b2dc409
MartinPucan <pucan.m@seznam.cz>
2020-11-22 21:03:42 +0100

    create project for panicum documentation

---
+++ src/content/index.js
@@ -0,0 +1,48 @@
+export default {
+  nav: {
+    logo: 'Panicum',
+    links: [
+      { text: 'Work', to: 'mywork' },
+      { text: 'Contact', to: 'mycontact' },
+    ],
+  },
+  header: {
+    img:
+      'https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80',
+    text: ['Hi!', "This is panicum.", "It's"],
+    typical: [
+      'lightweight',
+      1500,
+      'simple',
+      1500,
+      'powerful',
+      1500,
+			'easy',
+			2500,
+    ],
+    btnText: 'Discover More',
+  },
+
+  stack: {
+    title: 'Stack',
+    tech: [
+      {
+        img: process.env.PUBLIC_URL + '/assets/mongo.png',
+        alt: 'mongodb',
+      },
+      {
+        img: process.env.PUBLIC_URL + '/logo512.png',
+        alt: 'react',
+      },
+      {
+        img: process.env.PUBLIC_URL + '/assets/express.png',
+        alt: 'express',
+      },
+      {
+        img: process.env.PUBLIC_URL + '/assets/node.png',
+        alt: 'node',
+      },
+    ],
+    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only`,
+  },
+};
